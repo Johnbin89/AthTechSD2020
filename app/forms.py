309453e1576963174945aadd6381bdb3f9b5ac14
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-03-27 18:07:33 +0200

    register works / flash message on errors

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=200)
+    email = forms.EmailField(max_length=200, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
