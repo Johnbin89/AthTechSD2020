877466cc4a0981d8cf93c9523e9a3e66746da59d
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-05-13 22:48:33 +0300

    add esyd, ypan in decorators

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -4,11 +4,39 @@ from django.contrib.auth.decorators import user_passes_test
 
 def foreas_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
     '''
-    Decorator for views that checks that the logged in user is a student,
+    Decorator for views that checks that the logged in user is foreas,
     redirects to the log-in page if necessary.
     '''
     actual_decorator = user_passes_test(
         lambda u: u.is_active and u.is_foreas,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+def ypan_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    '''
+    Decorator for views that checks that the logged in user is xeiristis ypourgeiou,
+    redirects to the log-in page if necessary.
+    '''
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.is_ypan,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+def esyd_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    '''
+    Decorator for views that checks that the logged in user is esyd,
+    redirects to the log-in page if necessary.
+    '''
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.is_esyd,
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
