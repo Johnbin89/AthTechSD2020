009c6dddc40e7b7ad8811d9ac91ac09be756bebd
unknown <alexandra.mastrodimou@gmail.com>
2020-05-17 01:38:21 +0300

    Check if foreas has account information before adding an application

--- applications/views.py
+++ applications/views.py
@@ -13,18 +13,23 @@ from django.contrib.auth.decorators import login_required
 from accounts.decorators import foreas_required, ypan_required, esyd_required
 # def esydApp(request):
 #     return render(request, 'esydApp.html', {})
-
+from accounts.models import ApplicantProfile
+from django.contrib import messages
 
 @foreas_required
 def esydApp(request):
     form = UploadDocumentForm()
     pendingApps = ApplicationForm.objects.filter(foreas = request.user.id)
 
-    if request.method == 'POST':
-        form = UploadDocumentForm(request.POST, request.FILES)
-        form.instance.foreas = request.user
-        if form.is_valid():
-            # Do something with our files or simply save them
-            # if saved, our files would be located in media/ folder under the project's base folder
-            form.save()
+    userProfile = ApplicantProfile.objects.filter(user =  request.user.id)
+    if userProfile[0].has_empty_fields() :
+        messages.error(request, "Συμπληρώστε τα στοιχεία σας στην ενότητα Your Account")
+    else:
+        if request.method == 'POST':
+            form = UploadDocumentForm(request.POST, request.FILES)
+            form.instance.foreas = request.user
+            if form.is_valid():
+                # Do something with our files or simply save them
+                # if saved, our files would be located in media/ folder under the project's base folder
+                form.save()
     return render(request, 'esydApp.html', locals())
