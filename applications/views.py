79f6c20ea258e51a7397e5ec8d66861d69fb6086
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-05-17 16:49:10 +0300

    esyd form with subfields which are not in pending esyd applications

--- applications/views.py
+++ applications/views.py
@@ -5,19 +5,19 @@ from django.views.generic import DetailView
 from .models import ApplicationForm
 from django.contrib.auth.decorators import login_required
 from accounts.decorators import foreas_required, ypan_required, esyd_required
-from accounts.models import ApplicantProfile
+from accounts.models import ApplicantProfile, SubField
 from django.contrib import messages
 from django.utils.html import format_html
+from django.http import HttpResponseRedirect
 
 
 
 @foreas_required
 def esyd_for_foreas(request):
-    form = UploadDocumentForm()
-    esyd_page = True
+    context = {'esyd_page': True}
     pendingApps = ApplicationForm.objects.filter(foreas = request.user.id)
-
     userProfile = ApplicantProfile.objects.filter(user =  request.user.id)
+    form = UploadDocumentForm(current_user=request.user)
     if userProfile[0].has_empty_fields() :
         account_message = format_html("{} <a href='{}'>{}</a>",
                       "Συμπληρώστε τα στοιχεία σας στην ενότητα ", 
@@ -26,13 +26,13 @@ def esyd_for_foreas(request):
         messages.error(request, account_message)
     else:
         if request.method == 'POST':
-            form = UploadDocumentForm(request.POST, request.FILES)
+            form = UploadDocumentForm(request.POST, request.FILES, current_user = request.user)
             form.instance.foreas = request.user
             if form.is_valid():
-                # Do something with our files or simply save them
-                # if saved, our files would be located in media/ folder under the project's base folder
                 form.save()
-    return render(request, 'esydApp.html', locals())
+                return HttpResponseRedirect(reverse('esyd'))
+    context['form'],context['pendingApps'] = form, pendingApps
+    return render(request, 'esydApp.html', context)
 
 def ypan_application(request):
     context = {'ypan_page': True}
