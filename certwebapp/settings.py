8216a609d0638a9cdf04de51f91c1ee8f36d1885
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-05-17 20:09:00 +0300

    heroku

--- certwebapp/settings.py
+++ certwebapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 '''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
