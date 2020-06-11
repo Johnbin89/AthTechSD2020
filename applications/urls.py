ce4cd4b0e2780f5ebc92cfd86f5a3d51ee0c19cd
unknown <alexandra.mastrodimou@gmail.com>
2020-06-12 00:24:37 +0300

    Ypan application

--- applications/urls.py
+++ applications/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('foreas-esyd/', views_applications.esyd_for_foreas, name='esyd_for_foreas'),
     path('xeiristis-esyd/', views_applications.esyd_xeiristis, name='esyd_xeiristis'),
     path('ypan_application/', views_applications.ypan_application, name='ypan_application'),
+    path('xeiristis-ypan/', views_applications.ypan_xeiristis, name='ypan_xeiristis'),
     path('home/', views_applications.user_home, name='user_home_page'),
     path('updatetest/', views_applications.updateSub_onEsyd, name='esyd_update_subfield')
 ]
