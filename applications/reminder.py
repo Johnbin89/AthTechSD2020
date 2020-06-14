72e540aeab05d2031df4d5a108e397d2525f698c
unknown <alexandra.mastrodimou@gmail.com>
2020-06-14 14:46:40 +0300

    remove msg from index/ correct email reminder

--- applications/reminder.py
+++ applications/reminder.py
@@ -10,17 +10,19 @@ def run_reminder():
     print('start')
 
     oneMonthDate = date.today() + relativedelta(months=1)
-    threeMonthDate = date.today() + relativedelta(months=2)
+    threeMonthDate = date.today() + relativedelta(months=3)
+    print(oneMonthDate)
+    print(threeMonthDate)
 
     for ypanApp in ApplicationYpanForm.objects.filter(status ='Εγκρίθηκε'):
 
         for subField in ypanApp.ypan_subfields.filter(expDate = oneMonthDate):
 
             userEmail = subField.application.foreas.email
-            subField =  subField.subField.subField
+            field =  subField.subField.subField
             regulation = subField.subField.regulation.regulation
             send_mail('Προειδοποίηση Λήξης',
-                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το Υπουργείο Ανάπτυξης για το θεματικό πεδίο "' + subField + '" της νομοθεσίας "'+ regulation +'" λήγει σε 1 μήνα.',
+                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το Υπουργείο Ανάπτυξης για το θεματικό πεδίο "' + field + '" της νομοθεσίας "'+ regulation +'" λήγει σε 1 μήνα.',
                     'ypan.info@gmail.com',
                     [userEmail])
 
@@ -29,11 +31,11 @@ def run_reminder():
         for subField in ypanApp.ypan_subfields.filter(expDate = threeMonthDate):
 
             userEmail = subField.application.foreas.email
-            subField =  subField.subField.subField
+            field =  subField.subField.subField
             regulation = subField.subField.regulation.regulation
 
             send_mail('Προειδοποίηση Λήξης',
-                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το Υπουργείο Ανάπτυξης για το θεματικό πεδίο "' + subField + '" της νομοθεσίας "'+ regulation +'" λήγει σε 3 μήνες.',
+                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το Υπουργείο Ανάπτυξης για το θεματικό πεδίο "' + field + '" της νομοθεσίας "'+ regulation +'" λήγει σε 3 μήνες.',
                     'ypan.info@gmail.com',
                     [userEmail])
 
@@ -43,10 +45,10 @@ def run_reminder():
         for subField in esydApp.children.filter(expDate = oneMonthDate):
 
             userEmail = subField.application.foreas.email
-            subField =  subField.subField.subField
+            field =  subField.subField.subField
             regulation = subField.subField.regulation.regulation
             send_mail('Προειδοποίηση Λήξης',
-                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το ΕΣΥΔ για το θεματικό πεδίο "' + subField + '" της νομοθεσίας "'+ regulation +'" λήγει σε 1 μήνα.',
+                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το ΕΣΥΔ για το θεματικό πεδίο "' + field + '" της νομοθεσίας "'+ regulation +'" λήγει σε 1 μήνα.',
                     'ypan.info@gmail.com',
                     [userEmail])
 
@@ -55,32 +57,34 @@ def run_reminder():
         for subField in esydApp.children.filter(expDate = threeMonthDate):
 
             userEmail = subField.application.foreas.email
-            subField =  subField.subField.subField
+            field =  subField.subField.subField
             regulation = subField.subField.regulation.regulation
 
             send_mail('Προειδοποίηση Λήξης',
-                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το Υπουργείο Ανάπτυξης για το θεματικό πεδίο "' + subField + '" της νομοθεσίας "'+ regulation +'" λήγει σε 3 μήνες.',
+                    'Σας υπενθυμίζουμε ότι η αναγνώριση σας από το ΕΣΥΔ για το θεματικό πεδίο "' + field + '" της νομοθεσίας "'+ regulation +'" λήγει σε 3 μήνες.',
                     'ypan.info@gmail.com',
                     [userEmail])
 
 
     for ypanApp in ApplicationYpanForm.objects.filter(status ='Εγκρίθηκε',Civil_ExpDate = oneMonthDate): 
 
-        userEmail = ypanApp.foreas.userEmail
+        userEmail = ypanApp.foreas.email
 
         send_mail('Προειδοποίηση Λήξης',
-                'Σας υπενθυμίζουμε ότι το ασφαλιστήριο συμβόλαιο αστικής επαγγελματικής ευθύνης που καταθέσατε στην αίτηση σας με αριθμό ' + ypanApp.id + ' στο Υπουργείο Ανάπτυξης λήγει σε 1 μήνα.',
+                'Σας υπενθυμίζουμε ότι το ασφαλιστήριο συμβόλαιο αστικής επαγγελματικής ευθύνης που καταθέσατε στην αίτηση σας με αριθμό ' + str(ypanApp.id) + ' στο Υπουργείο Ανάπτυξης λήγει σε 1 μήνα.',
                 'ypan.info@gmail.com',
                 [userEmail])
 
 
     for ypanApp in ApplicationYpanForm.objects.filter(status ='Εγκρίθηκε',Civil_ExpDate = threeMonthDate):
         
-            userEmail = ypanApp.foreas.email
+        userEmail = ypanApp.foreas.email
 
-            send_mail('Προειδοποίηση Λήξης',
-                    'Σας υπενθυμίζουμε ότι το ασφαλιστήριο συμβόλαιο αστικής επαγγελματικής ευθύνης που καταθέσατε στην αίτηση σας με αριθμό ' + ypanApp.id + ' στο Υπουργείο Ανάπτυξης λήγει σε 3 μήνες.',
-                    'ypan.info@gmail.com',
-                    [userEmail])
+        send_mail('Προειδοποίηση Λήξης',
+                'Σας υπενθυμίζουμε ότι το ασφαλιστήριο συμβόλαιο αστικής επαγγελματικής ευθύνης που καταθέσατε στην αίτηση σας με αριθμό ' + str(ypanApp.id) + ' στο Υπουργείο Ανάπτυξης λήγει σε 3 μήνες.',
+                'ypan.info@gmail.com',
+                [userEmail])
+
+    print('end')
 
                 
