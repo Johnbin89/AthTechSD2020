470384d255dbd91557c05b6445c27fd42d437590
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-06-18 13:31:56 +0300

    remove form object from bacgkround task

--- applications/views.py
+++ applications/views.py
@@ -146,13 +146,13 @@ def ypan_application(request):
         messages.error(request, account_message)
     else:
         @background(schedule=1)
-        def handle_post_ypan(form):
+        def handle_post_ypan():
             if form.is_valid():
                 form.save()
         if request.method == 'POST':
             form = UploadYpanDocumentForm(request.POST, request.FILES, current_user = request.user)
             form.instance.foreas = request.user
-            handle_post_ypan(form)
+            handle_post_ypan()
             return HttpResponseRedirect(reverse('ypan_application'))
     context['form'],context['pendingApps'] = form, pendingApps
     return render(request, 'ypan_application.html', context)
