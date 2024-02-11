99af184542337122f669d0c9a94e996559fcb8cb
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-02-11 16:20:14 +0200

    #18 add Subfield of Regulations to admin view

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, ApplicantProfile, XeiristisYpourgeiou, XeiristisEsyd, Regulation
+from .models import User, ApplicantProfile, XeiristisYpourgeiou, XeiristisEsyd, Regulation, SubField
 # Register your models here.
 admin.site.register(User)
 admin.site.register(ApplicantProfile)
 admin.site.register(XeiristisEsyd)
 admin.site.register(XeiristisYpourgeiou)
 admin.site.register(Regulation)
+admin.site.register(SubField)
