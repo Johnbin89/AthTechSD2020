76396ea94e6748de4d357b04dc397187df7d1900
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-02-11 16:05:18 +0200

    #18 add Regulations to admin view

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, ApplicantProfile, XeiristisYpourgeiou, XeiristisEsyd
+from .models import User, ApplicantProfile, XeiristisYpourgeiou, XeiristisEsyd, Regulation
 # Register your models here.
 admin.site.register(User)
 admin.site.register(ApplicantProfile)
 admin.site.register(XeiristisEsyd)
 admin.site.register(XeiristisYpourgeiou)
+admin.site.register(Regulation)
