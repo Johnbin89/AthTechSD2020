cb404cd9f111c3517a22395eb4034a38b287f25e
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-05-10 13:24:48 +0300

    #25 fix DateField render/bs4_form

--- applications/forms.py
+++ applications/forms.py
@@ -20,6 +20,7 @@ class UploadDocumentForm(ModelForm):
         self.fields['file'].widget = forms.TextInput(attrs={
             'name': 'file',
             })
+        self.fields['file'].label = 'Αρχείο απόδειξης τεχνικής επάρκειας προσωπικού'
 
 class EsydStatusForm(ModelForm):
     class Meta:
@@ -47,9 +48,7 @@ class UploadYpanDocumentForm(ModelForm):
 
     class Meta:
         model = ApplicationYpanForm
-        fields = ['subfields', 'Non_Bankruptcy_Cert_1','Non_Bankruptcy_Cert_2','Non_Clearance_Cert','Non_Force_Arrange_Cert'
-        ,'GEMI_Cert','Tax_Clear_Cert','Insurance_Liability_Cert','Civil_ID','Civil_ExpDate','Balance_Sheet_3Y','Art_Of_Incorporation'
-            ,'GEMI_NoMod_Cert','Foreign_Activity_Decl','Coord_Group_Decl']
+        fields = ['subfields', 'Civil_ExpDate' ] + ApplicationYpanForm.get_file_strs()
     
     #find the subfields that are not in pending esyd apps for this user
     def __init__(self, *args, **kwargs):
@@ -57,10 +56,16 @@ class UploadYpanDocumentForm(ModelForm):
         super(UploadYpanDocumentForm, self).__init__(*args, **kwargs)
         esyd_subfields = ApplicationForm.objects.values_list('subfields', flat=True).filter(foreas = current_user.id,status='Εγκρίθηκε')
         used_subfields = ApplicationYpanForm.objects.values_list('subfields', flat=True).filter(foreas = current_user.id)
-        Notrejected_fields = used_subfields.exclude(status= 'Απορρίφθηκε')
+        notrejected_fields = used_subfields.exclude(status= 'Απορρίφθηκε')
         available_fields = SubField.objects.filter( pk__in=set(esyd_subfields))
-        final_fields = available_fields.exclude( pk__in=set(Notrejected_fields))
+        final_fields = available_fields.exclude( pk__in=set(notrejected_fields))
         self.fields['subfields'].queryset = final_fields
+        for field_str in ApplicationYpanForm.get_file_strs():
+            self.fields[field_str].widget = forms.TextInput(attrs={
+                'name': field_str,
+                })
+            self.fields[field_str].label = ApplicationYpanForm.get_verbose_names().get(field_str)
+
 
 class YpanStatusForm(ModelForm):
     class Meta:
--- applications/models.py
+++ applications/models.py
@@ -23,7 +23,7 @@ class ApplicationForm(models.Model):
     ]
     status = models.CharField(max_length=30, verbose_name='Κατασταση', choices=status_esyd_choices, default='Σε εκκρεμότητα')
     date = models.DateField(default=datetime.date.today, verbose_name='Ημ/νία υποβολής')
-    file = models.ForeignKey(StoredUpload, on_delete=models.CASCADE)
+    file = models.OneToOneField(StoredUpload, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse('Application', args=[self.pk])
@@ -66,22 +66,57 @@ class ApplicationYpanForm(models.Model):
     ]
     status = models.CharField(max_length=30, verbose_name='Κατασταση', choices=status_ypan_choices, default='Σε εκκρεμότητα')
     date = models.DateField(default=datetime.date.today, verbose_name='Ημ/νία υποβολής')
-    Non_Bankruptcy_Cert_1= models.FileField(upload_to=generate_filename_ypan, verbose_name="Πιστοποιητικό Πρωτοδικείου περί μη πτώχευσης σε ισχύ")
-    Non_Bankruptcy_Cert_2= models.FileField(upload_to=generate_filename_ypan, verbose_name="Πιστοποιητικό Πρωτοδικείου περί μη κατάθεσης αίτησης πτωχεύσεως σε ισχύ")
-    Non_Clearance_Cert= models.FileField(upload_to=generate_filename_ypan, verbose_name="Πιστοποιητικό Πρωτοδικείου περί μη θέσης σε εκκαθάριση σε ισχύ")
-    Non_Force_Arrange_Cert= models.FileField(upload_to=generate_filename_ypan, verbose_name="Πιστοποιητικό Πρωτοδικείου περί μη θέσης σε αναγκαστική διαχείριση σε ισχύ")
-    GEMI_Cert= models.FileField(upload_to=generate_filename_ypan, verbose_name="Πιστοποιητικό ΓΕΜΗ Γενικής Χρήσης και Εκπροσώπησης")
-    Tax_Clear_Cert = models.FileField(upload_to=generate_filename_ypan, verbose_name="Αποδεικτικό ενημερότητας για χρέη προς το Δημόσιο")
-    Insurance_Liability_Cert= models.FileField(upload_to=generate_filename_ypan, verbose_name="Βεβαίωση ΙΚΑ περί ασφαλιστικής ενημερότητας σε ισχύ")
-    Civil_ID= models.FileField(upload_to=generate_filename_ypan, verbose_name="Βεβαίωση ασφάλισης επαγγελματικής αστικής ευθύνης για την κάλυψη κινδύνων των παρεχόμενων υπηρεσιών")
+    Non_Bankruptcy_Cert_1= models.OneToOneField(StoredUpload, related_name="non_bank_app_1", on_delete=models.CASCADE)
+    Non_Bankruptcy_Cert_2= models.OneToOneField(StoredUpload, related_name="non_bank_app_2", on_delete=models.CASCADE)
+    Non_Clearance_Cert= models.OneToOneField(StoredUpload, related_name="non_clear_app", on_delete=models.CASCADE)
+    Non_Force_Arrange_Cert= models.OneToOneField(StoredUpload, related_name="non_forge_app", on_delete=models.CASCADE)
+    GEMI_Cert= models.OneToOneField(StoredUpload, related_name="gemi_cert_app", on_delete=models.CASCADE)
+    Tax_Clear_Cert = models.OneToOneField(StoredUpload, related_name="tax_clear_app", on_delete=models.CASCADE)
+    Insurance_Liability_Cert= models.OneToOneField(StoredUpload, related_name="ins_lia_app", on_delete=models.CASCADE)
+    Civil_ID= models.OneToOneField(StoredUpload, related_name="civil_id_app", on_delete=models.CASCADE)
     Civil_ExpDate = models.DateField(verbose_name='Ημ/νία λήξης ασφάλισης')
-    Balance_Sheet_3Y = models.FileField(upload_to=generate_filename_ypan, verbose_name="Ισολογισμοί τριών τελευταίων ετών")
-    Art_Of_Incorporation= models.FileField(upload_to=generate_filename_ypan, verbose_name="Κωδικοποιημένο καταστατικό της εταιρείας")
-    GEMI_NoMod_Cert= models.FileField(upload_to=generate_filename_ypan, verbose_name="Βεβαίωση περί μη τροποποίησης του πιστοποιητικού ΓΕΜΗ")
-    Foreign_Activity_Decl= models.FileField(upload_to=generate_filename_ypan, verbose_name="Υπεύθυνη δήλωση του κοινοποιημένου οργανισμού για τη δραστηριοποίησή του ή μη σε άλλες χώρες")
-    Coord_Group_Decl= models.FileField(upload_to=generate_filename_ypan, verbose_name="Υπεύθυνη δήλωση του κοινοποιημένου φορέα για τη συμμετοχή του ή μη στις ομάδες συντονισμού")
+    Balance_Sheet_3Y = models.OneToOneField(StoredUpload, related_name="bal_sheet_app", on_delete=models.CASCADE)
+    Art_Of_Incorporation= models.OneToOneField(StoredUpload, related_name="art_ink_app", on_delete=models.CASCADE)
+    GEMI_NoMod_Cert= models.OneToOneField(StoredUpload, related_name="gemi_nomod_app", on_delete=models.CASCADE)
+    Foreign_Activity_Decl= models.OneToOneField(StoredUpload, related_name="for_act_app", on_delete=models.CASCADE)
+    Coord_Group_Decl= models.OneToOneField(StoredUpload, related_name="cord_group_app", on_delete=models.CASCADE)
     def get_absolute_url(self):
         return reverse('ypan_application', args=[self.pk])
+    
+    @classmethod
+    def get_file_strs(cls)-> list[str]:
+        return ['Civil_ID',
+                'Non_Bankruptcy_Cert_1',
+                'Non_Bankruptcy_Cert_2',
+                'Non_Force_Arrange_Cert',
+                'Non_Clearance_Cert',
+                'GEMI_Cert',
+                'Tax_Clear_Cert',
+                'Insurance_Liability_Cert',
+                'Balance_Sheet_3Y',
+                'Art_Of_Incorporation',
+                'GEMI_NoMod_Cert',
+                'Foreign_Activity_Decl',
+                'Coord_Group_Decl'
+                ]
+
+    @classmethod
+    def get_verbose_names(cls)-> dict:
+        return {
+           'Non_Bankruptcy_Cert_1': "Πιστοποιητικό Πρωτοδικείου περί μη πτώχευσης σε ισχύ",
+           'Non_Bankruptcy_Cert_2': "Πιστοποιητικό Πρωτοδικείου περί μη κατάθεσης αίτησης πτωχεύσεως σε ισχύ",
+           'Non_Clearance_Cert': "Πιστοποιητικό Πρωτοδικείου περί μη θέσης σε εκκαθάριση σε ισχύ",
+           'Non_Force_Arrange_Cert': "Πιστοποιητικό Πρωτοδικείου περί μη θέσης σε αναγκαστική διαχείριση σε ισχύ",
+           'GEMI_Cert': "Πιστοποιητικό ΓΕΜΗ Γενικής Χρήσης και Εκπροσώπησης",
+           'Tax_Clear_Cert': "Αποδεικτικό ενημερότητας για χρέη προς το Δημόσιο",
+           'Insurance_Liability_Cert': "Βεβαίωση ΙΚΑ περί ασφαλιστικής ενημερότητας σε ισχύ",
+           'Civil_ID': "Βεβαίωση ασφάλισης επαγγελματικής αστικής ευθύνης για την κάλυψη κινδύνων των παρεχόμενων υπηρεσιών",
+           'Balance_Sheet_3Y': "Ισολογισμοί τριών τελευταίων ετών",
+           'Art_Of_Incorporation': "Κωδικοποιημένο καταστατικό της εταιρείας",
+           'GEMI_NoMod_Cert': "Βεβαίωση περί μη τροποποίησης του πιστοποιητικού ΓΕΜΗ",
+           'Foreign_Activity_Decl': "Υπεύθυνη δήλωση του κοινοποιημένου οργανισμού για τη δραστηριοποίησή του ή μη σε άλλες χώρες",
+           'Coord_Group_Decl': "Υπεύθυνη δήλωση του κοινοποιημένου φορέα για τη συμμετοχή του ή μη στις ομάδες συντονισμού",
+        }
 
 #subfields of an application to ypan
 class ApplicationYpanSubField(models.Model):
